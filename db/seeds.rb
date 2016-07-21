0a50e6423199fc967e8c2e21cceee6eed806285d
isratmir <isratmir@yandex.com>
2016-07-21 20:46:46 +0600

    updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-#AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
-=begin
+AdminUser.create!(email: 'admin@example.com', password: '123456', password_confirmation: '123456')
+
 sites = ActiveSupport::JSON.decode(File.read('db/seeds/websites.json'))
 
 sites.each do |site|
@@ -33,4 +33,3 @@ cats = [
     ]
 
 cats.map {|i| Category.create(i) }
-=end
